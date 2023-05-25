4be455f2dbcecaaf54bff5b759f661967cf9dcf2
Alisher-Dyushaliev <132102752+Alisher-Dyushaliev@users.noreply.github.com>
2023-05-25 12:59:14 +0600

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -88,6 +88,12 @@ DATABASES = {
         # 'USER': 'omega',
         # 'PASSWORD': '811',
         # 'HOST': 'localhost',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'mysitedb',
+        # 'USER': 'alisher',
+        # 'PASSWORD': 'test1234',
+        # 'HOST': 'localhost',
+        # 'PORT': '',
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
