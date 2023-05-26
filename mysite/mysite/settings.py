0346bdc694c236f63e8ab5866ee2ec10f48bc808
Alisher-Dyushaliev <132102752+Alisher-Dyushaliev@users.noreply.github.com>
2023-05-27 00:50:45 +0600

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -149,8 +149,8 @@ INTERNAL_IPS = ['127.0.0.1']
 
 EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = 465   # 2525
-EMAIL_HOST_USER = 'omega_kid@mail.ru'
-EMAIL_HOST_PASSWORD = 'D6yyjrk8PAcxxdB8mASq'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
